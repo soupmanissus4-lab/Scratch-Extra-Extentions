b038bc42040ccede21d1f2d80273e024c388371b
soupmanissus4-lab <soupmanissus4@gmail.com>
2026-01-22 08:30:21 -0500

    Update sensingplus.js

--- sensingplus.js
+++ sensingplus.js
@@ -24,7 +24,7 @@ getInfo() {
             ],
             "menus": {
                 "pressReleaseMenu": [{text:"press",value:1}, {text:"release",value:0}],
-                "keysOnKeyBoard": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "", "|", ";", ":", "'", """, ",", "<", ".", ">", "/", "?", "`", "~", "Esc", "Tab", "Caps Lock", "Shift", "Ctrl", "Fn", "Windows", "Alt", "Space", "Backspace", "Enter", "Delete", "Insert"]
+                "keysOnKeyBoard": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Shift", "Alt", "Space"]
             }  
         };
     }
