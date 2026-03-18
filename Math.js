88b7462fccfb23bfb9b8a541fe5cc99e8e30149a
soupmanissus4-lab <soupmanissus4@gmail.com>
2026-03-17 20:05:11 -0400

    Update Math.js

--- Math.js
+++ Math.js
@@ -1,50 +1,67 @@
-class math {
+class MathExtension {
     getInfo() {
         return {
-            "id": "Math",
-            "name": "Math",
-            "blocks": [
-                {
-                     opcode: "pi",
-                     blockType: "reporter",
-                     text: "pi",
-                     arguments: {}
-                },
-                {
-                    opcode: "whenIsEqual",
-                    blockType: "boolean",
-                    text: "is [number] [operation] [numberTwo] is equal to [answer]",
-                    arguments: {
-                        number: { type:"number", defaultValue:2},
-                        operation: { type:"string", menu: 'operation'},
-                        numberTwo: { type:"number", defaultValue:3},
-                        answer: { type:"number", defaultValue:6},
-                    }
+            id: 'mathextension',
+            name: 'Math',
+            blocks: [{
+                opcode: 'pi',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'pi',
+                arguments: {}
+            }, {
+                opcode: 'whenIsEqual',
+                blockType: Scratch.BlockType.BOOLEAN,
+                text: 'is [number] [operation] [numberTwo] = [answer] ?',
+                arguments: {
+                    number: {
+                        type: Scratch.ArgumentType.NUMBER,
+                        defaultValue: 2
+                    },
+                    operation: {
+                        type: Scratch.ArgumentType.STRING,
+                        menu: 'operations'
+                    },
+                    numberTwo: {
+                        type: Scratch.ArgumentType.NUMBER,
+                        defaultValue: 3
+                    },
+                    answer: {
+                        type: Scratch.ArgumentType.NUMBER,
+                        defaultValue: 6
+                    },
                 }
-            ],
+            }],
             menus: {
-                operations: {acceptReporters: false, items: ['x','/','-','+']}
+                operations: {
+                    acceptReporters: false,
+                    items: ['*', '/', '-', '+']
+                }
             }
-            
-        }
-    }
-    pi(args){
-        return 3.14159265357
+        };
     }
 
-    whenIsEqual(args){
-        const num = args.number;
+    pi(args) {
+        return 3.14159265357;
+    }
+
+    whenIsEqual(args) {
+        const num = parseFloat(args.number);
         const operation = args.operation;
-        const otherNum = args.numberTwo;
-        const answer = args.answer;
-        switch(operation){
-            case "x": {return num*otherNum==answer}
-            case "/": {return num/otherNum==answer}
-            case "-": {return num-otherNum==answer}
-            case "+": {return num+otherNum==answer}
+        const otherNum = parseFloat(args.numberTwo);
+        const answer = parseFloat(args.answer);
+
+        switch (operation) {
+            case '*':
+                return (num * otherNum) === answer;
+            case '/':
+                return (num / otherNum) === answer;
+            case '-':
+                return (num - otherNum) === answer;
+            case '+':
+                return (num + otherNum) === answer;
         }
         return false;
     }
 }
 
-Scratch.extensions.register(new math())
+Scratch.extensions.register(new MathExtension());
